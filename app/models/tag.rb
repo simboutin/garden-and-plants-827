55f9b326dedad4307599246da01f8500f180e9e3
Simon Boutin <simboutin@gmail.com>
2022-03-10 19:08:54 -0400

    allow user to destroy a plant or a tag relatively associated

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   TAG_NAMES = %w[fruit tree flower].freeze
 
-  has_many :plant_tags
+  has_many :plant_tags, dependent: :destroy
   has_many :plants, through: :plant_tags
 
   validates :name, presence: true, inclusion: { in: TAG_NAMES }
